ea78d48cfdd06421c536d7ce87ed2df6d222e66a
leveauapolline <leveauapolline@gmail.com>
2026-09-18 10:46:03 +0000

    TP3

--- data/init_db.sql
+++ data/init_db.sql
@@ -12,4 +12,19 @@ CREATE TABLE player (
     access_token VARCHAR(255)
 );
 
+-----------------------------------------------------
+-- Game
+-----------------------------------------------------
 
+
+DROP TABLE IF EXISTS game;
+
+CREATE TABLE game (
+    id_game      SERIAL PRIMARY KEY,
+    id_player1   INTEGER REFERENCES player(id_player),
+    id_player2   INTEGER REFERENCES player(id_player),
+    game_mode    VARCHAR(20),
+    id_winner    INTEGER REFERENCES player(id_player),
+    detail       VARCHAR(100),
+    timestamp    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
